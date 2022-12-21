aeff2fbdca28d64e207cfa405a1d9cbb22001c6c
MotyaMiracle <motyamiracle@gmail.com>
2022-12-21 10:25:27 +0300

    Connect and create database

---
+++ EFCore_Learning/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace EFCore_Learning
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                // создаем два объекта
+                User tom = new User { Name = "Tom", Age = 33 };
+                User alice = new User { Name = "Alice", Age = 26 };
+                
+                // добавляем объекты в бд
+                db.Users.Add(tom);
+                db.Users.Add(alice);
+                db.SaveChanges();
+                Console.WriteLine("Данные успешно добавлены");
+
+                // получаем данные из бд
+                var users = db.Users.ToList();
+                foreach (User u in users)
+                {
+                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+                }
+            }
+        }
+    }
+}
