cebf9742d482a3d0f5b074ba04f51bfcfa14728b
MotyaMiracle <motyamiracle@gmail.com>
2022-12-23 11:09:47 +0300

    Done

--- EFCore_Learning/ApplicationContext.cs
+++ EFCore_Learning/ApplicationContext.cs
@@ -9,13 +9,18 @@ namespace EFCore_Learning
 {
     public  class ApplicationContext : DbContext
     {
-        public DbSet<User> Users => Set<User> ();
+        public DbSet<User> Users { get; set; } = null!;
 
-        public ApplicationContext() => Database.EnsureCreated();
+        public ApplicationContext() 
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlite("Data Source = helloapp.db");
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=userdb;Username=postgres;Password=322228");
+
         }
     }
 }
--- EFCore_Learning/User.cs
+++ EFCore_Learning/User.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EFCore_Learning
 {
     public class User
     {
+        [Column("user_id")] // Column("user_id") означает что свойство Id будет сопоставляться со столбцом "user_id"
         public int Id { get; set; }
         public string? Name { get; set; }
         public int Age { get; set; }
