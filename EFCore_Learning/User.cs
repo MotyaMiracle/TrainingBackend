6694c2a33d0dddd545e26dccbe08ce785e49b966
MotyaMiracle <motyamiracle@gmail.com>
2022-12-23 11:54:32 +0300

    Work with [NotMapped] and Entity<>().Ignore(), ignore concrete property in table

--- EFCore_Learning/User.cs
+++ EFCore_Learning/User.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EFCore_Learning
 {
     public class User
     {
+        string name;
         public int Id { get; set; }
-        public string? Name { get; set; }
+        // Необязательно использовать автосвойства, можно и с полноценными блоками get, set
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
         public int Age { get; set; }
+        [NotMapped] // Исключение свойства при помощи аннотации
+        public string? Address { get; set; }
     }
 }
