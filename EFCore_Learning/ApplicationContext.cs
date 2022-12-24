55961ba51626572b671e7064ec5258221de50882
MotyaMiracle <MotyaMiracle@gmail.com>
2022-12-24 17:55:57 +0300

    Work with annotation [Table()], [Column()] and Fluent API ToTable(), Property().HasColumnName()

--- EFCore_Learning/ApplicationContext.cs
+++ EFCore_Learning/ApplicationContext.cs
@@ -20,6 +20,13 @@ namespace EFCore_Learning
         {
             optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=userdb;Username=postgres;Password=322228");
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<User>().ToTable("People");
+            //schema можно определить схему, к которой будет принадлежать таблица
+            //modelBuilder.Entity<User>().ToTable("People", schema: "userstore");
+            modelBuilder.Entity<User>().Property(u => u.Id).HasColumnName("user_id");
+        }
         
     }
 }
--- EFCore_Learning/User.cs
+++ EFCore_Learning/User.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EFCore_Learning
 {
+    [Table("Person")] // Через аннотацию
     public class User
     {
+        //[Column("user_id")]
         public int Id { get; set; }
         public string? Name { get; set; }
         public int Age { get; set; }
+        
     }
 }
