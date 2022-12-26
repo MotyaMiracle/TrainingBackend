a95c4bca860cc8fdecd728c847e550ce1b13f614
MotyaMiracle <MotyaMiracle@gmail.com>
2022-12-26 21:52:22 +0300

    Work with .Where().Load(), .Entry().Collection().Load(), .Entry().Reference().Load()

--- EFCore_Learning/Company.cs
+++ EFCore_Learning/Company.cs
@@ -6,12 +6,10 @@ using System.Threading.Tasks;
 
 namespace EFCore_Learning
 {
-    public class User
+    public class Company
     {
         public int Id { get; set; }
         public string? Name { get; set; }
-
-        public int? CompanyId { get; set; }
-        public Company? Company { get; set; }
+        public List<User> Users { get; set; } = new();
     }
 }
--- EFCore_Learning/Program.cs
+++ EFCore_Learning/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace EFCore_Learning
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCore_Learning
 {
     public class Program
     {
@@ -6,21 +8,46 @@
         {
             using (ApplicationContext db = new ApplicationContext())
             {
-                // создаем два объекта
-                User tom = new User { Name = "Tom", Age = 33 };
-                User alice = new User { Name = "Alice", Age = 26 };
+                db.Database.EnsureDeleted();
+                db.Database.EnsureCreated();
+
+                Company microsoft = new Company { Name= "Microsoft" };
+                Company google = new Company { Name = "Google" };
+                db.Companies.AddRange(microsoft, google);
                 
-                // добавляем объекты в бд
-                db.Users.Add(tom);
-                db.Users.Add(alice);
+                User tom = new User { Name = "Tom", Company = microsoft };
+                User sam = new User { Name = "Sam", Company = google };
+                User alice = new User { Name = "Alice", Company = microsoft };
+                User kate = new User { Name = "Kate", Company = google };
+                db.Users.AddRange(tom, sam, alice, kate);
+                
                 db.SaveChanges();
                 Console.WriteLine("Данные успешно добавлены");
-
-                // получаем данные из бд
-                var users = db.Users.ToList();
-                foreach (User u in users)
+            }
+            using(ApplicationContext db = new ApplicationContext())
+            {
+                Company? company = db.Companies.FirstOrDefault();
+                if (company != null)
                 {
-                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+                    //db.Users.Where(u => u.CompanyId == company.Id).Load(); // Если без условия то можно было db.Users.Load()
+                    //Метод Collection применяется, если навигационное свойство представляет коллекцию
+                    db.Entry(company).Collection(c => c.Users).Load();
+
+                    Console.WriteLine($"Company: {company.Name}");
+                    foreach(var user in company.Users)
+                    {
+                        Console.WriteLine($"User: {user.Name}");
+                    }
+                }
+            }
+            using(ApplicationContext db = new ApplicationContext())
+            {
+                User? user = db.Users.FirstOrDefault();
+                if(user != null)
+                {
+                    //Если навигационное свойство представляет одиночный объект, то можно применять метод Reference:
+                    db.Entry(user).Reference(u => u.Company).Load();
+                    Console.WriteLine($"{user.Name} - {user.Company?.Name}");
                 }
             }
         }
