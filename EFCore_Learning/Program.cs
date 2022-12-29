9cbde8e73c112a24a0bd0319a6ce38d5a877fc34
MotyaMiracle <motyamiracle@gmail.com>
2022-12-29 14:13:27 +0300

    Work with where, select and .Where

--- EFCore_Learning/Program.cs
+++ EFCore_Learning/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace EFCore_Learning
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+
+namespace EFCore_Learning
 {
     public class Program
     {
@@ -6,21 +9,48 @@
         {
             using (ApplicationContext db = new ApplicationContext())
             {
-                // создаем два объекта
-                User tom = new User { Name = "Tom", Age = 33 };
-                User alice = new User { Name = "Alice", Age = 26 };
-                
-                // добавляем объекты в бд
-                db.Users.Add(tom);
-                db.Users.Add(alice);
+                db.Database.EnsureDeleted();
+                db.Database.EnsureCreated();
+
+                Company microsoft = new Company { Name= "Microsoft" };
+                Company google = new Company { Name = "Google" };
+                db.Companies.AddRange(microsoft, google);
+
+                User tom = new User { Name = "Tom", Age = 33, Company= microsoft };
+                User sam = new User { Name = "Sam", Age = 27, Company= google };
+                User alice = new User { Name = "Alice", Age = 26, Company = microsoft };
+                User kate = new User { Name = "Kate", Age = 25, Company = google };
+                db.Users.AddRange(tom, sam, alice, kate);
+
                 db.SaveChanges();
                 Console.WriteLine("Данные успешно добавлены");
 
-                // получаем данные из бд
-                var users = db.Users.ToList();
-                foreach (User u in users)
+            }
+            // Использование некоторых операторов LINQ
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                var users = (from user in db.Users.Include(p => p.Company)
+                             where user.CompanyId == 1
+                             select user).ToList();
+                foreach(var user in users)
                 {
-                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+                    Console.WriteLine($"{user.Name} ({user.Age}) - {user.Company?.Name}");
+                }
+                Console.WriteLine();
+            }
+            // Использование методов расширения LINQ
+            using( ApplicationContext db = new ApplicationContext())
+            {
+                var users = db.Users.Include(p => p.Company).Where(p => p.CompanyId == 1);
+                foreach (var user in users)
+                {
+                    Console.WriteLine($"{user.Name} ({user.Age}) - {user.Company?.Name}");
+                }
+                Console.WriteLine();
+                var companies = db.Companies.Include(c => c.Users).Where(c => c.Name == "Tom");
+                foreach (var company in companies)
+                {
+                    Console.WriteLine($"{company.Name}");
                 }
             }
         }
